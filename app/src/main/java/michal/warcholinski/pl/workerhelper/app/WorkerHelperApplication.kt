d5b60a754de5a41709dbed08723980f29544d8b6
Michal <Michal>
2022-02-17 21:11:35 +0100

    Application class

---
+++ app/src/main/java/michal/warcholinski/pl/workerhelper/app/WorkerHelperApplication.kt
@@ -0,0 +1,11 @@
+package michal.warcholinski.pl.workerhelper.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Michał Warcholiński on 2021-12-23.
+ */
+@HiltAndroidApp
+class WorkerHelperApplication : Application() {
+}
